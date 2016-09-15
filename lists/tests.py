3af728f867cbd253bff08775101d9f354ed84dc3
Daniel Martinez <dmpinero@gmail.com>
2016-09-15 19:12:01 +0200

    Test funcional casi terminados

--- lists/tests.py
+++ lists/tests.py
@@ -12,8 +12,25 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    # La respuesta a la request / (método home_page) es la página home.html
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        #self.assertEqual(response.content.decode(), expected_html)
+
+    # Guardar petición POST
+    def test_home_page_can_save(self):
+        request = HttpRequest()                        # Establecer petición
+        request.method = 'POST'                        # Establecer método de la petición
+        request.POST['item_text'] = 'A new list item'  # Almacenar dentro de la petición un elemento con un valor
+
+        response = home_page(request)                  # Obtener plantilla a través de la vista (controlador)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': request.POST['item_text']},
+            request=request
+        )
+        #self.assertEqual(response.content.decode(), expected_html)
